dd79bfb05793cd223cb9cfcee1874e7184fb66bd
Yuji Masujima <masjima2712@gmail.com>
2020-01-16 16:57:40 +0900

    記事投稿用

---
+++ src/test/java/SpringLogin/app/SpringLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringLogin.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
